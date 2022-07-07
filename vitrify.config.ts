e6dbf1f6b50cc9c54658c13bcf57c143a269509a
Stefan van Herwijnen <stefan@devliert.info>
2022-07-07 12:12:12 +0200

    fix: exclude quasar-ui-qmediaplayer from optimizeDeps

--- vitrify.config.ts
+++ vitrify.config.ts
@@ -1,55 +1,58 @@
-import ssg from './vite-plugin-ssg.js'
+import ssg from "./vite-plugin-ssg.js";
 // import { defineConfig } from 'vite'
-import { resolvePackageData } from 'vite'
+import { resolvePackageData } from "vite";
 const qMarkdown = async ({ app }) => {
-  const Plugin = (await import('@quasar/quasar-ui-qmarkdown')).default
-  app.use(Plugin)
-}
+  const Plugin = (await import("@quasar/quasar-ui-qmarkdown")).default;
+  app.use(Plugin);
+};
 
 const qMediaplayer = async ({ app }) => {
-  const Plugin = (await import('@stefanvh/quasar-ui-qmediaplayer')).default
+  const Plugin = (await import("@stefanvh/quasar-ui-qmediaplayer")).default;
 
-  app.use(Plugin)
-}
+  app.use(Plugin);
+};
 export default function ({ mode, command }) {
   return {
     vitrify: {
       sass: {
         additionalData: [
           `@import '@quasar/quasar-ui-qmarkdown/src/QMarkdown.sass'`,
-          `@import '@stefanvh/quasar-ui-qmediaplayer/src/QMediaPlayer.sass'`
-        ]
+          `@import '@stefanvh/quasar-ui-qmediaplayer/src/QMediaPlayer.sass'`,
+        ],
       },
       hooks: {
-        onBoot: [qMarkdown, qMediaplayer]
-      }
+        onBoot: [qMarkdown, qMediaplayer],
+      },
     },
     plugins: [
       ssg(),
       {
-        name: 'resolve',
+        name: "resolve",
         config: (config) => {
           return {
             resolve: {
               alias: [
                 {
-                  find: new RegExp('^@quasar/quasar-ui-qmarkdown'),
+                  find: new RegExp("^@quasar/quasar-ui-qmarkdown"),
                   replacement: resolvePackageData(
-                    '@quasar/quasar-ui-qmarkdown',
+                    "@quasar/quasar-ui-qmarkdown",
                     config.vitrify.urls.app.pathname
-                  ).dir
-                }
-              ]
-            }
-          }
-        }
-      }
+                  ).dir,
+                },
+              ],
+            },
+          };
+        },
+      },
     ],
+    optimizeDeps: {
+      exclude: ["@stefanvh/quasar-ui-qmediaplayer"],
+    },
     quasar: {
-      extras: ['material-icons', 'fontawesome-v5'],
+      extras: ["material-icons", "fontawesome-v5"],
       framework: {
-        plugins: ['AppFullscreen', 'Meta']
-      }
-    }
-  }
+        plugins: ["AppFullscreen", "Meta"],
+      },
+    },
+  };
 }
